73d873ada9f24ac9072ebeedf7fa74db0212dcb4
Hovhannes Poghosyan <hovhannes.poghosyan@icloud.com>
2021-05-28 13:39:40 +0400

    Humanize Security Task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import Countries from './Countries';
 import './App.css';
+import {useState} from "react";
+
 
 function App() {
+
+    const [filter, setFilter] = useState();
+
+    const filteredCountries = [];
+
+
+    function onChangeHandler (e) {
+        setFilter(e.target.value);
+    }
+
+    if(filter) {
+        for(let i = 0; i < Countries.length; ++i){
+            if(Countries[i].name.includes(filter)){
+                filteredCountries.push(Countries[i]);
+            }
+        }
+    }
+
+    // function outputCountries () {
+    //     if(filteredCountries.length = 0) {
+    //         return (
+    //             <ul className={"countryList"}>
+    //                 {
+    //                     Countries.map((item) => <li className={"singleCountry"} key={item.code}>{item.name}</li>)
+    //                 }
+    //             </ul>
+    //         )
+    //     } else {
+    //         return (
+    //             <ul className={"countryList"}>
+    //                 {
+    //                     filteredCountries.map((item) => <li className={"singleCountry"} key={item.code}>{item.name}</li>)
+    //                 }
+    //             </ul>
+    //         )
+    //     }
+    // }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Filtered list</h1>
+      <input type="text" className={"search"} placeholder={"Filter ..."} onChange={onChangeHandler}/>
+        <ul className={"countryList"}>
+            {
+                filteredCountries.length === 0 ?
+                    Countries.map((item) => <li className={"singleCountry"} key={item.code}>{item.name}</li>):
+                    filteredCountries.map((item) => <li className={"singleCountry"} key={item.code}>{item.name}</li>)
+            }
+        </ul>
     </div>
   );
 }
